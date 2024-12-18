92e7e0b1e8ef6a5328c63174a2588b9892a9c47c
Arif Imeri <arifimeri.11@gmail.com>
2024-12-18 19:59:12 +0100

    added 10 new rapppers

--- app/src/main/java/com/example/rappersratingapp/MainActivity.kt
+++ app/src/main/java/com/example/rappersratingapp/MainActivity.kt
@@ -28,11 +28,11 @@ class MainActivity : AppCompatActivity() {
         topButton = findViewById(R.id.topButton)
         bottomButton = findViewById(R.id.bottomButton)
 
-        // Sample rappers data
+
         rappers.add(Rapper(1, "Eminem", "Mathers", "Eminem","Lose Yourself", 100000, imageUrl = R.drawable.eminem))
         rappers.add(Rapper(2, "Jay-Z", "Carter", "Jay-Z", "Empire State of Mind", 80000, imageUrl = R.drawable.jayz))
         rappers.add(Rapper(3, "Rigels", "Rajku", "Noizy","Toto", 300000, imageUrl = R.drawable.rigels))
-        rappers.add(Rapper(4, "Arkimed", "Lushaj", "Stresi", "Loco", 240000, R.drawable.arkimed))
+        rappers.add(Rapper(4, "Arkimed", "Lushaj", "Stresi", "Loco", 240000, R.drawable.rigels))
         rappers.add(Rapper(5, "Melida", "Ademi", "Melinda", "Alkool", 200000, R.drawable.melinda))
         rappers.add(Rapper(6, "Doruntina", "Shala", "Tayna", "Edhe Ti", 180000, R.drawable.tayna))
         rappers.add(Rapper(7, "Gramoz" ,"Aliu", "Mozzik", "Ti amo", 165000, imageUrl = R.drawable.mozzik))
@@ -40,9 +40,19 @@ class MainActivity : AppCompatActivity() {
         rappers.add(Rapper(9, "Kljedi", "Dogjani", "Finem", "Bling Bling", 130000, imageUrl = R.drawable.finem))
         rappers.add(Rapper(10, "Elvana", "Gjata", "Elvana", "Njesoj", 123000, imageUrl = R.drawable.elvana))
         rappers.add(Rapper(11, "Dafina", "Zeqiri", "Dafina", "Dafine Moj", 100000, imageUrl = R.drawable.dafina))
+        rappers.add(Rapper(12, "Drake", "Graham", "Drake", "God's Plan", 900000, imageUrl = R.drawable.drake))
+        rappers.add(Rapper(13, "Kanye", "West", "Kanye West", "Stronger", 950000, imageUrl = R.drawable.kanye))
+        rappers.add(Rapper(14, "Travis", "Scott", "Travis Scott", "SICKO MODE", 850000, imageUrl = R.drawable.travis))
+        rappers.add(Rapper(15, "Nicki", "Minaj", "Nicki Minaj", "Anaconda", 1200000, imageUrl = R.drawable.nicki))
+        rappers.add(Rapper(16, "Lil", "Wayne", "Lil Wayne", "Lollipop", 1000000, imageUrl = R.drawable.lilwayne))
+        rappers.add(Rapper(17, "Cardi", "B", "Cardi B", "I Like It", 950000, imageUrl = R.drawable.cardi))
+        rappers.add(Rapper(18, "Post", "Malone", "Post Malone", "Rockstar", 950000, imageUrl = R.drawable.postmalone))
+        rappers.add(Rapper(19, "J Cole", "Cole", "J Cole", "Middle Child", 800000, imageUrl = R.drawable.jcole))
+        rappers.add(Rapper(20, "Lil", "Uzi Vert", "Lil Uzi Vert", "XO Tour Llif3", 880000, imageUrl = R.drawable.liluzi))
+        rappers.add(Rapper(21, "Snoop", "Dogg", "Snoop Dogg", "Drop It Like It's Hot", 1100000, imageUrl = R.drawable.snoop))
 
 
-        // Add more rappers data here
+
 
         val adapter = RapperAdapter(this, rappers)
         rappersListView.adapter = adapter
